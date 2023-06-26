955b61e72b4f9b77df75c12911742a9178c43297
Yura Movsesyan <movsesyan-03@inbox.ru>
2023-06-26 19:07:09 +0300

    added test

--- new_class.sh
+++ new_class.sh
@@ -1,3 +1,9 @@
 touch ./source/header/$1.h
 echo "#include \"./header/$1.h\"
 #include <stdlib.h>" > ./source/$1.c
+
+echo "#include <assert.h>
+#include <stdio.h>" > ./test/header/$1_test.h
+
+echo "#include \"./header/$1_test.h\"
+#include \"../source/header/$1.h\"" > ./test/$1_test.c
